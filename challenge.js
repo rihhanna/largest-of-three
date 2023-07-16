9a38fdbf23e62bc039a207fcb3284685a9233163
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-15 23:35:09 -0700

    answera of three number largest

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const number1=10
+const number2=20
+const number3=40
+if(number1>number2 && number1<number3){
+    console.log(number1 ,'is less then' ,number2 ,'and', number3)
+}else if(number2<number1 || number2>number3){
+    console.log(number2 ,"is greater then",number1,'and less then',number3)
+}else{
+    console.log('The largest number out of the three numbers is', number3)
+}
